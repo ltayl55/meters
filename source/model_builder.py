a4e561447e93adbfc086351b4098652b6654491c
Luke Taylor <ltayl55@lsu.edu>
2023-03-03 18:04:12 -0600

    Tried out my model with different hyperparameters.

--- source/model_builder.py
+++ source/model_builder.py
@@ -2,7 +2,7 @@
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from sklearn.metrics import accuracy_score
-
+import pandas as pd
 
 from source.data_preprocess import DataPreprocessing
 
@@ -32,3 +32,48 @@ class ModelBuilder(DataPreprocessing):
         self.accuracy = accuracy_score(y_test, DT_predicted)
 
         return DT_classifier
+    
+    import pandas as pd
+
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+class PreProcess():
+    def __init__(self):
+        pass
+
+    def load_data(self, path):
+        data = pd.read_csv(path, sep="\t", header=None).dropna()
+
+        print(data.head())
+
+        data = data.to_numpy()
+
+        return data
+
+class ANN(PreProcess):
+    def __init__(self, *args, **kwargs):
+        super(ANN, self).__init__(*args, **kwargs)
+
+    def myANN(self, myX_train,  myX_test, myy_train, myy_test):
+
+        mlp = MLPClassifier(hidden_layer_sizes=(1, 3), learning_rate_init=0.09, max_iter=2500)
+
+        mlp.fit(myX_train, myy_train)
+
+        mlp_predicted = mlp.predict(myX_test)
+
+        myerror = 0
+        for i in range(len(myy_test)):
+            myerror += np.sum(mlp_predicted != myy_test)
+
+        mytotal_accuracy = 1 - myerror / len(myy_test)
+
+        self.accuracy = accuracy_score(myy_test, mlp_predicted)
+
+        return mlp
